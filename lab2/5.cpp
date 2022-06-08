9bba9d6f884794094bc900643b5b7b59066bc7af
Kashish Bataju <kashishbataju999@gmail.com>
2022-06-08 08:40:51 +0545

    Minor change

--- lab2/5.cpp
+++ lab2/5.cpp
@@ -10,7 +10,7 @@ inline float netPayment(float sal);
 int main()
 {
     float salary;
-    std::cout<<"Enter you salary: ";
+    std::cout<<"Enter your salary: ";
     std::cin>>salary;
     std::cout<<"After the deduction of income tax, you will receive Rs. "<<netPayment(salary)<<"."<<std::endl;
     return 0;

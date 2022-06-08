f408f5b273d6f407635511b9e442d77626a4fc29
Kashish Bataju <kashishbataju999@gmail.com>
2022-06-08 08:26:12 +0545

    Added lab 2 four.cpp

---
+++ lab2/4.cpp
@@ -0,0 +1,31 @@
+//Lab-2 Question-4
+
+/*A function that passes two temperatures by reference and sets the larger of the two numbers 
+to a value entered by user by using return by reference.*/
+
+#include<iostream>
+
+float& setLargerTemperature(float& temp1, float& temp2);
+
+int main()
+{
+    float temperature1, temperature2, setToThisTemp;
+    std::cout<<"Enter temperature 1: ";
+    std::cin>>temperature1;
+    std::cout<<"Enter temperature 2: ";
+    std::cin>>temperature2;
+    std::cout<<"Enter the temperature you want to set the larger one to: ";
+    std::cin>>setToThisTemp;
+    std::cout<<"Before calling the function the tempeartures are "<<temperature1<<" and "<<temperature2<<"."<<std::endl;
+    setLargerTemperature(temperature1, temperature2) = setToThisTemp;
+    std::cout<<"After calling the function the tempeartures are "<<temperature1<<" and "<<temperature2<<"."<<std::endl;
+    return 0;
+}
+
+float& setLargerTemperature(float& temp1, float& temp2)
+{
+    if(temp1>temp2)
+        return temp1;
+    else
+        return temp2;
+}

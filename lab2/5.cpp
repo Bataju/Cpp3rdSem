25e4f1917d32afad759ca8e937528c6aee2e0a78
Kashish Bataju <kashishbataju999@gmail.com>
2022-06-08 08:39:47 +0545

    Added lab 2 five.cpp

---
+++ lab2/5.cpp
@@ -0,0 +1,22 @@
+//Lab-2 Question-5
+
+/*Assume that employee will have to pay 10 percent income tax to the government. 
+Ask user to enter the employee salary. Use inline function to display the net payment to the employee by the company.*/
+
+#include<iostream>
+
+inline float netPayment(float sal);
+
+int main()
+{
+    float salary;
+    std::cout<<"Enter you salary: ";
+    std::cin>>salary;
+    std::cout<<"After the deduction of income tax, you will receive Rs. "<<netPayment(salary)<<"."<<std::endl;
+    return 0;
+}
+
+inline float netPayment(float sal)
+{
+    return 0.9*sal;
+}

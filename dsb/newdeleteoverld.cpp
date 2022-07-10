33a74f232598263a11646a5dcae4794317376ffe
Kashish Bataju <kashishbataju999@gmail.com>
2022-07-10 11:04:15 +0545

    Some practice qns

--- dsb/newdeleteoverld.cpp
+++ dsb/newdeleteoverld.cpp
@@ -7,41 +7,41 @@ private:
     int x, y;
 
 public:
-    Cpoint(int a=0, int b=0):x(a), y(b){}
+    Cpoint(int a = 0, int b = 0) : x(a), y(b) {}
     void display()
     {
-        cout<<"("<<this->x<<" , "<<this->y<<")"<<endl;
+        cout << "(" << this->x << " , " << this->y << ")" << endl;
     }
-    void *operator new(size_t);//must have void* return type
+    void *operator new(size_t); // must have void* return type
     void *operator new[](size_t);
-    void operator delete(void *);//must have void return type
+    void operator delete(void *); // must have void return type
     void operator delete[](void *);
 };
 
-void* Cpoint::operator new(size_t size)
+void *Cpoint::operator new(size_t size)
 {
-    cout<<"Overloaded operator new called..."<<endl;
-    Cpoint *ptr = (Cpoint*)malloc(size);
+    cout << "Overloaded operator new called..." << endl;
+    Cpoint *ptr = (Cpoint *)malloc(size);
     return ptr;
 }
 
-void* Cpoint::operator new[](size_t size)
+void *Cpoint::operator new[](size_t size)
 {
-    cout<<"Overloaded operator new called..."<<endl;
-    Cpoint *ptr = ::new Cpoint[size/sizeof(Cpoint)];//size = sizeof(Cpoint) * n i.e. n = size/sizeof(Cpoint)
+    cout << "Overloaded operator new called..." << endl;
+    Cpoint *ptr = ::new Cpoint[size / sizeof(Cpoint)]; // size = sizeof(Cpoint) * n i.e. n = size/sizeof(Cpoint)
     return ptr;
 }
 
 void Cpoint::operator delete(void *ptr)
 {
-    cout<<"Overloaded operator delete called..."<<endl;
-    ::delete (Cpoint*)ptr;
+    cout << "Overloaded operator delete called..." << endl;
+    ::delete (Cpoint *)ptr;
 }
 
 void Cpoint::operator delete[](void *ptr)
 {
-    cout<<"Overloaded operator delete called..."<<endl;
-    ::delete[] (Cpoint*)ptr;
+    cout << "Overloaded operator delete called..." << endl;
+    ::delete[](Cpoint *) ptr;
 }
 
 int main()
@@ -51,15 +51,15 @@ int main()
     p2 = new Cpoint[5];
     (*p1).display();
 
-    for(int i=0; i<5; i++)
+    for (int i = 0; i < 5; i++)
     {
-        p2[i] = Cpoint(i+1, i+1);
+        p2[i] = Cpoint(i + 1, i + 1);
     }
-    for(int i=0; i<5; i++)
+    for (int i = 0; i < 5; i++)
     {
         p2[i].display();
     }
-    delete p1; 
+    delete p1;
     delete[] p2;
     return 0;
 }

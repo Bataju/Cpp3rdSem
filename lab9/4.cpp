2be1a49ab99e92ae7187499224453f5d8cac4f43
Kashish Bataju <kashishbataju999@gmail.com>
2022-08-21 08:32:36 +0545

    Done with lab 9

--- lab9/4.cpp
+++ lab9/4.cpp
@@ -3,7 +3,61 @@
 #include<iostream>
 using namespace std;
 
+class Factorial
+{
+    private:
+        float number;
+    public:
+        class NEGATIVE{};
+        class FLOATINGPOINT{};
+        void getNumber()
+        {
+            cout<<"Enter number: ";
+            cin>>number;
+        }
+        void displayFactorial()
+        {
+            if(number<0)
+                throw NEGATIVE();
+            if(static_cast<int>(number) != number)
+                throw FLOATINGPOINT();
+            long int facto = 1;
+            for(int i=1; i<=number; i++)
+            {
+                facto*=i;
+            }
+            cout<<facto<<endl;
+        }
+};
+
 int main()
 {
+    Factorial f1;
+    f1.getNumber();
+    try
+    {
+        try
+        {
+            f1.displayFactorial();
+        }
+        catch(Factorial::NEGATIVE)
+        {
+            cout<<"The factorial of a negative number doesn't exist."<<endl;
+            throw;
+        }
+        catch(Factorial::FLOATINGPOINT)
+        {
+            cout<<"The factorial of a floating point doesn't exist."<<endl;
+            throw;
+        }
+    }
+    catch(Factorial::NEGATIVE)
+    {
+        cout<<"Handling the exception rethrown."<<endl;
+    }
+    catch(...)
+    {
+        cout<<"Handling all the unhandled exceptions."<<endl;
+    } 
     return 0;
 }
